8750da44174fcce995c285babff2d9e1537a7e22
David Heyman <dave@axismaps.com>
2015-03-10 09:20:58 +0000

    Formatting

--- js/layers.js
+++ js/layers.js
@@ -1,21 +1,21 @@
-var layers = [
-	{
-		"table" : "city_council",
-		"name" : "City Council Districts",
-		"default": false
-	},
-	{
-		"table" : "school_districts",
-		"name" : "School Districts",
-		"default": false
-	},
-	{
-		"table" : "zips",
-		"name" : "Zip Codes",
-		"default": false
-	}
-],
-overlays;
+var overlays;
+	layers = [
+		{
+			"table" : "city_council",
+			"name" : "City Council Districts",
+			"default": false
+		},
+		{
+			"table" : "school_districts",
+			"name" : "School Districts",
+			"default": false
+		},
+		{
+			"table" : "zips",
+			"name" : "Zip Codes",
+			"default": false
+		}
+	];
 
 function init_layers( button ) {
 	//button should be a jquery object
