1e76d14fcac4dc6d39c8621646de30d673447877
Josh Ryan <mr.josh.j.ryan@gmail.com>
2015-03-24 13:50:42 -0500

    Load boundaries on click

--- js/process.js
+++ js/process.js
@@ -1,3 +1,25 @@
 function load_process_boundaries( properties, executing ){
-	console.log( properties );
+	// console.log( properties );
+	
+	var layerStyle = L.geoJson( null, {
+		style : function( feature ) {
+			return { 
+				color : '#000',
+				weight : 2
+			};
+    	},
+    	onEachFeature : function( feature, layer ) {
+	    	layer.on({
+				mouseover : highlightFeature,
+				mouseout : resetHighlight,
+				click : featureClick
+			});
+    	}
+	});
+	
+	process_neighborhoods = omnivore.topojson( endpoint + "/process/" + properties.id, null, layerStyle )
+		.on( 'ready', function() {
+			map.removeLayer( neighborhoods );
+		})
+		.addTo( map );
 }
